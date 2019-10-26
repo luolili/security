2cefb033b4fcee94afaea75adb673b28e8cfab4f
Administrator <2084267015@qq.com>
2019-10-26 16:36:10 +0800

    security

--- security-browser/src/main/java/com/luo/security/browser/MyUserDetailsService.java
+++ security-browser/src/main/java/com/luo/security/browser/MyUserDetailsService.java
@@ -2,17 +2,23 @@ package com.luo.security.browser;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyUserDetailsService implements UserDetailsService {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     /**
      * @param username 前台 传过来的
      */
@@ -25,7 +31,7 @@ public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         logger.info("username:" + username);
-        return new User(username, "123", true, true, true, false,
+        return new User(username, passwordEncoder.encode("123"), true, true, true, true,
                 AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
